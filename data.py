cdd30a57d65ba5c210c3c942e240c9a7b14130c9
NoorAbuElhija <102956206+NoorAbuElhija@users.noreply.github.com>
2022-04-23 04:33:30 +0300

    edit 2

--- data.py
+++ data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+
 def load_data(path, features):
 
     df = pd.read_csv(path)
@@ -10,39 +11,40 @@ def load_data(path, features):
 
 def filter_by_feature(data, feature, values):
 
-    data2 = {k: v.copy() for k,v in data.items()}
-    data1 = {k: v.copy() for k,v in data.items()}
+    data2 = {k: v.copy() for k, v in data.items()}
+    data1 = {k: v.copy() for k, v in data.items()}
     i = 0
     j = 0
-    l = 0
+    k = 0
     while i < len(data[feature]):
         if data[feature][i] in values:
             for key in data:
                 data2[key].pop(i-j)
-            j=j+1
+            j = j+1
         else:
             for key in data:
-                data1[key].pop(i-l)
-            l=l+1
+                data1[key].pop(i-k)
+            k = k+1
         i = i+1
     return data1, data2
+
 
 def print_details(data, features, statistic_functions):
 
     for key in data:
-        if(key in features):
-            print(key,end=": ")
+        if key in features:
+            print(key, end=": ")
             for i in range(len(statistic_functions)):
-                avg=statistic_functions[i](data[key])
-                formatf= round(avg,2)
-                if (i==0):
-                    print(formatf,end=", ")
+                avg = statistic_functions[i](data[key])
+                formatf = round(avg, 2)
+                if i == 0:
+                    print(formatf, end=", ")
                 else:
                     print(formatf)
 
 
 def print_joint_details(data, features, statistic_functions, statistic_functions_names):
     for i in range(len(statistic_functions)):
-         cov = statistic_functions[i](data[features[1]],data[features[0]])
-    format_float = round(cov,2)
+        result = statistic_functions[i](data[features[1]], data[features[0]])
+    format_float = round(result, 2)
     print(statistic_functions_names, format_float)
--- statistics.py
+++ statistics.py
@@ -1,36 +1,36 @@
 
 from math import sqrt
 
-def calc_mean(values):
 
-    i = 0
-    sums=0
+def calc_mean(values):
+    sums = 0
     for i in values:
-        sums=sums+i
-    n=len(values)
-    avg=sums/ n
+        sums = sums+i
+    n = len(values)
+    avg = sums / n
     return avg
 
+
 def calc_stdv(values):
-    avg=calc_mean(values)
-    n=0
+    avg = calc_mean(values)
     n = len(values)
-    sum=0
     for i in range(n):
-        sum=sum+(values[i]-avg)*(values[i]-avg)
+        sum = sum+(values[i]-avg)*(values[i]-avg)
 
     stdv = sqrt((1/(n-1))*sum)
     return stdv
+
+
 def calc_covariance(values1, values2):
 
-    avg1=calc_mean(values1)
-    avg2=calc_mean(values2)
-    sum=0
-    n=len(values2)
-    for i in range (n):
-        sum=sum+((values1[i]-avg1)*(values2[i]-avg2))
-    cov=1/(n-1)*sum
+    avg1 = calc_mean(values1)
+    avg2 = calc_mean(values2)
+    n = len(values2)
+    for i in range(n):
+        sum = sum+((values1[i]-avg1)*(values2[i]-avg2))
+    cov = 1/(n-1)*sum
     return cov
+
 
 def population_statistics(feature_description, data, treatment, target, threshold, is_above,
 statistic_functions):
@@ -52,8 +52,8 @@ statistic_functions):
     for j in range(len(statistic_functions)):
         result=statistic_functions[j](data1)
         formatf = round(result, 2)
-        if (k==0):
+        if k == 0:
             print(formatf, end=", ")
-            k=k+1
+            k = k+1
         else:
             print(formatf)
